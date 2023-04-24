728a22bbeb8774b143aeee892c5775f6aa763bb3
Int Fract <intfract@gmail.com>
2023-04-24 09:43:11 +0000

    created svelte app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from './Header.svelte'
+	import './styles.css'
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="IGCSE Kit" />
+</svelte:head>
+
+<section>
+	<h1>IGCSE Kit</h1>
+</section>
+
+<style>
+	
+</style>
