cc075001cbe6a070967ed35ca76795d9b088e16e
Bit Logist <thebitlogist@gmail.com>
2023-07-16 11:17:40 +0000

    removed test images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,7 @@
 
 	import Carousel from '$lib/components/Carousel.svelte'
 
-	let images = [
-		'https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-		'https://images.unsplash.com/photo-1610194352361-4c81a6a8967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
-		'https://images.unsplash.com/photo-1618202133208-2907bebba9e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-	]
+	let images = []
 </script>
 
 <svelte:head>
