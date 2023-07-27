e86735cf218271a0e0601a474bcfd68609067b2a
Int Fract <intfract@gmail.com>
2023-07-27 06:24:38 +0000

    added link tree

--- src/routes/igcse/+page.svelte
+++ src/routes/igcse/+page.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+  const modules = import.meta.glob('./**/+page.svelte')
+  let links: string[] = []
+
+  export let data
+
+  for (const key of Object.keys(modules)) {
+    links.push(key.replace('.', data.url).replace('/+page.svelte', ''))
+  }
+</script>
+
 <section>
   <h1>IGCSE</h1>
+  <h2>Link Tree</h2>
+  <ul>
+    {#each links as link}
+      <li>
+        <a href={link}>{link}</a>
+      </li>
+    {/each}
+  </ul>
 </section>
