79b93d66309e54cd1c2c4a8a66c0118c9d3ab012
Int Fract <intfract@gmail.com>
2023-07-30 22:14:46 +0000

    addded physics route

---
+++ src/routes/igcse/physics/+page.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+  import LinkTree from '$lib/components/LinkTree.svelte'
+
+  export let data
+</script>
+
+<section>
+  <h1>Physics</h1>
+  <LinkTree url={data.url} glob={import.meta.glob('./**/+page.svelte')}></LinkTree>
+</section>
