873af643a9961620c934dc4cd625fc1153cd93c9
Int Fract <intfract@gmail.com>
2023-08-01 20:40:52 +0000

    handled input

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -5,8 +5,12 @@
   export let glob: Record<string, () => Promise<unknown>>
   
   let query: string = ''
+
+  function handleInput(e: CustomEvent<any>) {
+    console.log(e)
+  }
 </script>
 
 <div class="search">
-  <Textfield bind:value={query} variant="filled" label="Search"></Textfield>
+  <Textfield bind:value={query} variant="filled" label="Search" on:input={handleInput}></Textfield>
 </div>
