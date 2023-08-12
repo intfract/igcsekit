3be361383b2e0c323d51f08d11ec1eaeaf1fd17b
Int Fract <intfract@gmail.com>
2023-08-12 10:53:06 +0000

    added boiling

--- src/routes/igcse/physics/thermal_physics/+page.svelte
+++ src/routes/igcse/physics/thermal_physics/+page.svelte
@@ -7,6 +7,11 @@
     ['Liquid', 'Weak', 'Medium', 'Move slowly and around other particles'],
     ['Gas', 'None', 'Large', 'Move quickly and collide with other particles'],
   ]
+
+  let b: [string, string, string][] = [
+    ['Boiling', 'Throughout', 'Specific'],
+    ['Evaporation', 'Surface', 'Any'],
+  ]
 </script>
 
 <section>
@@ -18,10 +23,10 @@
   <DataTable>
     <Head>
       <Row>
-        <Cell>Component</Cell>
-        <Cell>Abbreviation</Cell>
-        <Cell>Function</Cell>
-        <Cell>Function</Cell>
+        <Cell>State</Cell>
+        <Cell>Forces</Cell>
+        <Cell>Distances</Cell>
+        <Cell>Motion</Cell>
       </Row>
     </Head>
     <Body>
@@ -34,6 +39,20 @@
       {/each}
     </Body>
   </DataTable>
+  <h3>Changing State of Matter</h3>
+  <p>Melting, boiling and sublimation absorb thermal energy from the environment while freezing, condensation, and deposition release thermal energy into the environment. The <strong>temperature does not change</strong> during melting and boiling because the energy is being used to break the bonds between the particles.</p>
+  <ul>
+    <li>Melting: Solid to Liquid</li>
+    <li>Freezing: Liquid to Solid</li>
+  </ul>
+  <ul>
+    <li>Boiling: Liquid to Gas</li>
+    <li>Condensation: Gas to Liquid</li>
+  </ul>
+  <ul>
+    <li>Sublimation: Solid to Gas</li>
+    <li>Deposition: Gas to Liquid</li>
+  </ul>
   <h3>Evaporation</h3>
   <p>During evaporation, the most energetic molecules leave the surface of the liquid. This results in the cooling of the liquid because the <strong>average kinetic energy</strong> of the remaining particles decreases.</p>
   <p>The rate of evaporation can be increased by:</p>
@@ -42,6 +61,26 @@
     <li>surface area</li>
     <li>wind speed</li>
   </ul>
+  <h4>Evaporation and Boiling</h4>
+  <p>Boiling occurs throughout a liquid while evaporation occurs on the surface of a liquid. Unlike evaporation, boiling can only take place when the temperature reaches the boiling point or range of the substance.</p>
+  <DataTable>
+    <Head>
+      <Row>
+        <Cell>Process</Cell>
+        <Cell>Distribution</Cell>
+        <Cell>Temperature</Cell>
+      </Row>
+    </Head>
+    <Body>
+      {#each b as row}
+        <Row>
+          {#each row as cell}
+            <Cell>{cell}</Cell>
+          {/each}
+        </Row>
+      {/each}
+    </Body>
+  </DataTable>
   <h4>Sweating</h4>
   <p>Sweating cools down the body due to evaporation.</p>
   <ol>
