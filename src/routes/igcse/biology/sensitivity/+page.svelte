be11d1f7498ccc3bdd9d3772242303cb001c0be0
Int Fract <intfract@gmail.com>
2023-09-08 09:00:38 +0000

    added sensitivity

---
+++ src/routes/igcse/biology/sensitivity/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  //
+</script>
+
+<section>
+  <h1>Sensitivity</h1>
+  <h2>Nervous Control</h2>
+  <p>A nerve impulse is an <strong>electrical</strong> signal that passes along nerve cells called neurons.</p>
+  <h3>The Human Nervous System</h3>
+  <p>The nervous system can be split into 2 parts:</p>
+  <ul>
+    <li>
+      <strong>Central</strong>: brain and spinal cord
+    </li>
+    <li>
+      <strong>Peripheral</strong>: all the other nerves that connect to the central nervous system
+    </li>
+  </ul>
+  <h3>Reflex Arc</h3>
+  <p>There are 3 different types of neurons involved in the relfex arc.</p>
+  <ul>
+    <li>
+      <strong>Sensory</strong>: long and have a cell body branching off the middle of the axon
+    </li>
+    <li>
+      <strong>Relay</strong>: short and have a small cell body at one end with many dendrites branching off it
+    </li>
+    <li>
+      <strong>Motor</strong>: long and have a large cell body at one end with long dendrites branching off it
+    </li>
+  </ul>
+  <p>Relay neurons are located in the central nervous system and do not have myelin sheaths on their axons. Motor neurons are also located in the central nervous system.</p>
+  <p>Reflex arcs are a way of responding to stimuli using effectors such as muscles and glands.</p>
+  <h3>Voluntary and Involuntary Actions</h3>
+  <h2>Sense Organs</h2>
+  <h2>Hormones</h2>
+  <h2>Homeostasis</h2>
+  <h2>Tropic Responses</h2>
+</section>
