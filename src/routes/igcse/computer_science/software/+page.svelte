3c8828aad7fbb28bf55ec23d644251c5b9c9ad56
Int Fract <intfract@gmail.com>
2023-09-19 10:21:03 +0000

    edited software

--- src/routes/igcse/computer_science/software/+page.svelte
+++ src/routes/igcse/computer_science/software/+page.svelte
@@ -7,6 +7,7 @@
   <h1>Software</h1>
   <h2>Computer Software</h2>
   <p>System software is <strong>essential</strong> and includes the <strong>operating system</strong> and <strong>utility software</strong>. It is installed directly on the computer and manages the hardware. On the other hand, application software is optional and installed on top of system software to allow the user to perform specific tasks using the computer.</p>
+  <p>In short, system software provides what the <strong>computer</strong> requires while application software provides what the <strong>user</strong> requires.</p>
   <h3>Operating System</h3>
   <p>The operating system is booted after the BIOS has loaded successfully. An operating system is a program designed to manage hardware and software resources. It performs a variety of tasks:</p>
   <ul>
@@ -30,6 +31,13 @@
     <li>Disk Allocation</li>
   </ul>
   <h3>Application Software</h3>
+  <p>Application software provides services that are required for users to interact with the computer. Some examples include:</p>
+  <ul>
+    <li>web browsers</li>
+    <li>word processors</li>
+    <li>spreadsheet programs</li>
+    <li>video games</li>
+  </ul>
   <h3>Interrupts</h3>
   <p>Interrupts allow computers to multitask. When an interrupt is generated, the CPU stops processing the current task and services the interrupt. There are 2 types of interrupts:</p>
   <ul>
@@ -87,24 +95,47 @@
   <p>A translator converts source code written in a high-level or low-level language into machine code.</p>
   <p>There are 3 types of translators:</p>
   <ul>
-    <li>Assemblers</li>
-    <li>Compilers</li>
-    <li>Interpreters</li>
+    <li>assemblers</li>
+    <li>compilers</li>
+    <li>interpreters</li>
   </ul>
-  <h3>Assemblers</h3>
+  <h3>Assembler</h3>
   <p>An assembler translates low-level assembly code into machine code.</p>
-  <h3>Compilers</h3>
+  <h3>Compiler</h3>
   <p>A compiler translates a whole source code file into machine code in one go. It also:</p>
   <ul>
     <li>checks for errors <strong>before</strong> running the program</li>
     <li>generates a report of <strong>all the errors</strong> found in the program</li>
     <li>produces an executable file</li>
   </ul>
-  <h3>Interpreters</h3>
+  <h3>Interpreter</h3>
   <p>An interpreter translates source code one line after the other. It also:</p>
   <ul>
     <li>translates one line of source code into machine code and runs it <strong>before</strong> moving on to the next line</li>
     <li>stops when it encounters an error</li>
     <li>allows errors to be fixed in real-time</li>
   </ul>
+  <h2>Integrated Development Environment</h2>
+  <p>An IDE provides tools that make it easier for programmers to write code.</p>
+  <h3>Tools</h3>
+  <p>IDEs contain a lot of built-in tools. The functions of an IDE include:</p>
+  <ul>
+    <li>code editor</li>
+    <li>auto-completion</li>
+    <li>auto-correction</li>
+    <li>prettyprint</li>
+    <li>built-in interpreter</li>
+    <li>built-in compiler</li>
+    <li>run-time environment</li>
+  </ul>
+  <p>It may be useful to think of an IDE as having 2 layers.</p>
+  <ol>
+    <li>
+      <strong>Code Editor</strong>: automatically completes or corrects code as it is typed and uses prettyprint to display source code in an attractive way
+    </li>
+    <li>
+      <strong>Integrated Development Environment</strong>: adds interpreters to be used during development, compilers for producing a final executable, and a run-time environment to run the program
+    </li>
+  </ol>
+  <p>For example, Visual Studio Code is a code editor while Visual Studio is an IDE. This is because Visual Studio targets the <a href="https://dotnet.microsoft.com/en-us/" target="_blank">.NET framework</a> made by microsoft. It has an embedded compiler for C# and Visual Basic. Visual Studio Code on the other hand, requires you to install most resources.</p>
 </section>
