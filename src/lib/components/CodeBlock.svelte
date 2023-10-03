86ea3fd484153e06e4a6464815f347c13b4697b0
Int Fract <intfract@gmail.com>
2023-10-03 01:27:57 +0000

    created functioning terminal

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-  import Button, { Label } from '@smui/button'
+  import Button, { Label, Icon } from '@smui/button'
+  import Textfield from '@smui/textfield'
 
+  let vars: Record<string, any> = {}
   let terminal: string = ''
+  let value: string = ''
+  let index: number = 0
 
-  function execute(code: string) {
-    terminal = 'Hello, world!'
+  function output(...args: string[]) {
+    let s = ''
+    args.forEach(arg => s += arg)
+    terminal += s
+  }
+
+  let tasks: Function[] = [
+    (vars: Record<string, any>, input: string) => {
+      vars['name'] = input
+      output("Hello ", vars['name'], "!")
+    },
+  ] // dummy pseudocode compilation
+
+  function run(code: string) {
+    terminal = ''
+    // compile pseudocode to tasks
+  }
+
+  function submit(e: any) {
+    if (e.key !== 'Enter') return
+    terminal += value + '\n'
+    const task = tasks[index]
+    if (task) {
+      task(vars, value)
+      index++
+    }
   }
 
   export let code: string
@@ -15,12 +43,16 @@
     <pre>{code}</pre>
   </div>
   <div class="buttons">
-    <Button variant="raised" on:click={e => execute(code)}>
+    <Button variant="raised" on:click={e => run(code)}>
+      <Icon class="material-symbols-rounded">play_arrow</Icon>
       <Label>Run</Label>
     </Button>
   </div>
   <div class="terminal">
     <pre>{terminal}</pre>
+  </div>
+  <div class="input">
+    <Textfield label="Input" bind:value on:keydown={e => submit(e)} style="width: 100%;"></Textfield>
   </div>
 </div>
 
