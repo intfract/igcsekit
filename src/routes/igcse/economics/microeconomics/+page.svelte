fadc865691114593cf787f99cdcd453768596f97
Int Fract <intfract@gmail.com>
2023-11-01 05:51:18 +0000

    added money

--- src/routes/igcse/economics/microeconomics/+page.svelte
+++ src/routes/igcse/economics/microeconomics/+page.svelte
@@ -11,13 +11,31 @@
 
 <section>
   <h1>Microeconomics</h1>
+  <h2>Banking</h2>
+  <h3>Money</h3>
+  <p>Money has attributes such as:</p>
+  <ul>
+    <li>scarcity</li>
+    <li>uniformity</li>
+    <p>portability</p>
+    <li>divisibility</li>
+    <li>acceptability</li>
+    <li>durability</li>
+  </ul>
+  <p>Money has attributes such as:</p>
+  <ul>
+    <li>store of value</li>
+    <li>measure of value</li>
+    <li>standard of deferred payment</li>
+    <li>medium of exchange</li>
+  </ul>
   <h2>Firms</h2>
   <h3>Economies of Scale</h3>
   <p>Large firms benefit from economies of scale unlike smaller firms.</p>
   <DataTable>
     <Head>
       <Row>
-        <Cell>Topic</Cell>
+        <Cell>Economy</Cell>
         <Cell>Description</Cell>
       </Row>
     </Head>
