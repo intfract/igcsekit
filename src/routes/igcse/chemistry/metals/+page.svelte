ed09c6042fa97b4d3a74741e957d1d0a129340de
Int Fract <intfract@gmail.com>
2023-11-06 06:27:22 +0000

    added metals question

---
+++ src/routes/igcse/chemistry/metals/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  //
+</script>
+
+<section>
+  <h1>Metals</h1>
+  <h2>Properties</h2>
+  <p>Metals are:</p>
+  <ul>
+    <li>solids with high melting and boiling points</li>
+    <li>malleable</li>
+    <li>good conductors of electricity and heat</li>
+  </ul>
+  <h3>Alloys</h3>
+  <p>Alloys are mixtures of a metal with other elements.</p>
+  <p>Alloys are used instead of pure metals because they may be:</p>
+  <ul>
+    <li>more resistant to corrossion</li>
+    <li>stronger</li>
+    <li>harder</li>
+    <li>less dense</li>
+  </ul>
+  <p>Alloys contain atoms of <strong>different sizes</strong> which distorts the regular arrangement of atoms in metals. This makes it harder for layers to <strong>slide</strong> over each other.</p>
+</section>
