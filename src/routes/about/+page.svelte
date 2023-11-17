da0ccfcfdd858686d9a1a1e90b73036a0acff769
Bit Logist <thebitlogist@gmail.com>
2023-11-17 09:39:04 +0000

    updated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import Paper, { Title, Subtitle, Content } from '@smui/paper'
   import Ripple from '@smui/ripple'
+  import Chip, { Set, LeadingIcon, Text } from '@smui/chips'
 
   type Socials = {
-    github?: string,
+    github: string,
     discord?: string,
   }
 
   type Member = {
     name: string,
+    tags: string[],
     description: string,
     socials: Socials,
   }
@@ -16,13 +18,28 @@
   let members: Member[] = [
     {
       name: 'u_int',
-      description: 'Owner',
+      tags: ['Creator', 'Developer'],
+      description: 'The creator of IGCSE Kit and lead developer',
       socials: {
         github: 'intfract',
         discord: 'u_int',
       }
     },
+    {
+      name: 'dotVansh',
+      tags: ['Developer'],
+      description: 'Another developer committed to providing free IGCSE resources',
+      socials: {
+        github: 'intfract',
+        discord: 'mr.code5197',
+      }
+    },
   ]
+
+  let iconMap = {
+    'Creator': 'rocket_launch',
+    'Developer': 'code',
+  }
 </script>
 
 <section>
@@ -30,9 +47,19 @@
   <p>Meet the team!</p>
   <div class="papers">
     {#each members as member}
-      <a class="paper" use:Ripple={{ surface: true }}>
+      <a class="paper" use:Ripple={{ surface: true }} href={`https://github.com/${member.socials.github}`}>
         <Paper color="primary" variant="outlined">
-          <Title>{member.name}</Title>
+          <Title>
+            {member.name}
+            <Set chips={member.tags} let:chip class="inline-flex">
+              <Chip {chip} shouldRemoveOnTrailingIconClick={false}>
+                {#if Object.keys(iconMap).includes(chip)}
+                  <LeadingIcon class="material-symbols-rounded">{iconMap[chip]}</LeadingIcon>
+                {/if}
+                <Text tabindex={0}>{chip}</Text>
+              </Chip>
+            </Set>
+          </Title>
           <Content>{member.description}</Content>
         </Paper>
       </a>
