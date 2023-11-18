0491c3a85d750d34b3d1831617776e0a3a10849a
Bit Logist <thebitlogist@gmail.com>
2023-11-18 07:40:42 +0000

    edited sensitivity

--- src/routes/igcse/biology/sensitivity/+page.svelte
+++ src/routes/igcse/biology/sensitivity/+page.svelte
@@ -32,8 +32,49 @@
   <p>Relay neurons are located in the central nervous system and do not have myelin sheaths on their axons. Motor neurons are also located in the central nervous system.</p>
   <p>Reflex arcs are a way of responding to stimuli using effectors such as muscles and glands.</p>
   <h3>Voluntary and Involuntary Actions</h3>
+  <p>Voluntary actions require conscious thought whereas involuntary actions are automatic.</p>
   <h2>Sense Organs</h2>
+  <h3>Eye</h3>
+  <p>The eye is made up of many parts.</p>
+  <ul>
+    <li>
+      <strong>Cornea</strong>: refracts light
+    </li>
+    <li>
+      <strong>Iris</strong>: controls how much light enters pupil
+    </li>
+    <li>
+      <strong>Lens</strong>: focuses light onto retina
+    </li>
+    <li>
+      <strong>Retina</strong>: contains light receptors that are sensitive to light from different colours
+    </li>
+    <li>
+      <strong>Optic Nerve</strong>: carries impulses to the brain
+    </li>
+  </ul>
+  <p>The <strong>pupil reflex</strong> is used to control the amount of light entering the eye. The action of the radial and circular muscles is described as <strong>antagonistic</strong> because they work opposite to each other.</p>
+  <ul>
+    <li>
+      In bright light:
+      <ul>
+        <li>circular muscles contract</li>
+        <li>radial muscles relax</li>
+        <li>pupil size decreases</li>
+      </ul>
+    </li>
+    <li>
+      In dim light:
+      <ul>
+        <li>circular muscles relax</li>
+        <li>radial muscles contract</li>
+        <li>pupil size increases</li>
+      </ul>
+    </li>
+  </ul>
   <h2>Hormones</h2>
+  <p>A hormone is a chemical substance, produced by a gland and carried by the blood, which alters the activity of one or more specific target organs.</p>
   <h2>Homeostasis</h2>
+  <p>Homeostasis is the maintenance of a constant internal environment.</p>
   <h2>Tropic Responses</h2>
 </section>
