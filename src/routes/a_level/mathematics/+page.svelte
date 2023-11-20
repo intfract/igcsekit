00587794fd51cc9dd0440f0830485688b4ee6a12
Bit Logist <thebitlogist@gmail.com>
2023-11-20 07:44:33 +0000

    added a levels

---
+++ src/routes/a_level/mathematics/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import LinkTree from '$lib/components/LinkTree.svelte'
+
+  export let data
+</script>
+
+<section>
+  <h1>Mathematics</h1>
+  <LinkTree url={data.pathname} glob={import.meta.glob('./**/+page.svelte')}></LinkTree>
+</section>
