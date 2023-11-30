763b5b0f82342bd5b18a7ec3be78f28efc88c729
Int Fract <intfract@gmail.com>
2023-11-30 22:29:32 +0000

    added international trade

---
+++ src/routes/igcse/economics/international_trade/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  //
+</script>
+
+<section>
+  <h1>International Trade</h1>
+  <h2>Sepcialisation</h2>
+  <p>Specialisation is the concentration of resources on particular products or tasks.</p>
+  <h3>Advantages</h3>
+  <ul>
+    <li>Increase in quantity produced</li>
+    <li>Increase in quality of products</li>
+    <li>
+      Costs are cut due to economies of scale
+      <ul>
+        <li>Price can be lowered to compete</li>
+        <li>Price can be increased and customers will still exist</li>
+      </ul>
+    </li>
+    <li>
+      Increase in exports
+      <ul>
+        <li>Better balance of payments and balance of trade</li>
+        <li>Can import other goods</li>
+        <li>Increases global efficiency by globalisation</li>
+      </ul>
+    </li>
+  </ul>
+  <h2>Globalisation</h2>
+  <h2>Foreign Exchange Rate</h2>
+</section>
