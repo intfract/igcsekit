0b0c1359901c586117a30470756d49590c3c3faa
Bit Logist <thebitlogist@gmail.com>
2024-01-01 07:21:37 +0000

    added databases chaer

---
+++ src/routes/igcse/computer_science/databases/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  //
+</script>
+
+<section>
+  <h1>Databases</h1>
+  <h2>Table</h2>
+  <p>Databases are made up of tables. In IGCSE, databases only consist of a single table.</p>
+  <ul>
+    <li>Fields are the different categories or columns in a table which organise data belonging to the same group</li>
+    <li>Records are different groups of data or rows that often have a unique identifier</li>
+  </ul>
+</section>
