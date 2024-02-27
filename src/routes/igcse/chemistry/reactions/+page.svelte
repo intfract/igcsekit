80282135236f2b88cdd95686d8cc0892b1ee7ba7
Int Fract <intfract@gmail.com>
2024-02-27 07:29:09 +0000

    added reactions chapter

---
+++ src/routes/igcse/chemistry/reactions/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import Callout from '$lib/components/Callout.svelte'
+</script>
+
+<section>
+  <h1>Reactions</h1>
+  <h2>Rate of Reaction</h2>
+  <p>The rate of reaction is the speed of a chemical reaction. It is how quickly reactant particles are converted into product particles.</p>
+  <h3>Measuring Rate of Reaction</h3>
+  <p>Some reactions produce a gas. There are a few ways to measure the rate of reaction in this situation. It is possible:</p>
+  <ul>
+    <li>collect the gas in a syringe and record its volume over fixed time intervals</li>
+    <li>calculate the mass lost as gas over fixed time intervals</li>
+  </ul>
+  <p>Usually, it is easier to record the volume of gas instead of the mass lost.</p>
+  <h3>Interpreting Data</h3>
+  <p>Graphs are used to visualise the rate of reaction. The <strong>slope</strong> or <em>steepness</em> of the graphed curve represents the rate of reaction.</p>
+  <p>The graph of a typical reaction with <strong>amount of reactants</strong> plotted against <strong>time</strong> would look like an exponentially decreasing curve. This is because the rate of reaction decreases as the amount of reactants decreases. The opposite of this graph would be <strong>amount of products</strong> against <strong>time</strong>.</p>
+  <h3>Factors Affecting Rate of Reaction</h3>
+  <p>All of these factors increase the rate of reaction.</p>
+  <h4>Concentration</h4>
+  <ul>
+    <li>More reactant particles exist per unit volume</li>
+    <li>More collisions occur per second</li>
+  </ul>
+  <h4>Temperature</h4>
+  <ul>
+    <li>Particles have more kinetic energy</li>
+    <li>Particles collide with more force</li>
+    <li>More particles have enough activation energy to react</li>
+    <li>More <strong>successful</strong> collisions occur per second</li>
+  </ul>
+  <h4>Surface Area</h4>
+  <ul>
+    <li>More reactant particles are exposed to each other</li>
+    <li>More collisions occur per second</li>
+  </ul>
+  <Callout emoji="🤔">
+    It is good to finish explanations by stating whether the rate of reaction increases or not.
+  </Callout>
+  <h3>Dangers</h3>
+  <p>Increasing the rate of reaction can create a danger of explosive combustion with fine powders and gases.</p>
+  <h2>Redox</h2>
+  <p>A redox reaction involves both oxidation and reduction. Substitution reactions are a common example of redox reactions. A more reactive metal can react with an ionic compound and substitute itself for the less reactive metal. The more reactive metal loses electrons while the less reactive metal gains electrons.</p>
+  <div class="equation">
+    Mg + CuSO<sub>4</sub> → Cu + MgSO<sub>4</sub>
+  </div>
+  <Callout emoji="🤔">
+    OILRIG is a good acronym which stands for oxidation is loss (of electrons) and reduction is gain (of electrons).
+  </Callout>
+  <h3>Reduction</h3>
+  <p>Reduction can be described as the loss of oxygen or the gain of electrons.</p>
+  <div class="equation">
+    Cu<sup>2+</sup> + 2e<sup>-</sup> → Cu
+  </div>
+  <p>A reducing agent is a chemical that reduces another reactant. This means reducing agents lose electrons.</p>
+  <h3>Oxidation</h3>
+  <p>Oxidation can be describes as the gain of oxygen or the loss of electrons.</p>
+  <div class="equation">
+    Mg - 2e<sup>-</sup> → Mg<sup>2+</sup>
+  </div>
+  <p>A oxidising agent is a chemical that oxidises another reactant. This means reducing agents gain electrons.</p>
+</section>
