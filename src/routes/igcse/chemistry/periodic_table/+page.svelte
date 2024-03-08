b3ed1bb6ff5f4d35629df1924ba8c18ce69d11f0
Int Fract <intfract@gmail.com>
2024-03-08 10:53:32 +0000

    added group trends

--- src/routes/igcse/chemistry/periodic_table/+page.svelte
+++ src/routes/igcse/chemistry/periodic_table/+page.svelte
@@ -9,4 +9,19 @@
   <h2>Periodic Trends</h2>
   <p>Elements become more non-metallic across a period. Elements near the bottom left corner of the table are the most metallic while elements near the top right corner are the least metallic.</p>
   <p>An element's group number is equal to the number of electrons in its outer shell (valence electrons). For example, oxygen is in group 6 and has 6 valence electrons.</p>
+  <h2>Group Properties</h2>
+  <h3>Group 1</h3>
+  <p>This is a collection of soft metals that react with water to produce an alkali and hydrogen.</p>
+  <p>Going down a group:</p>
+  <ul>
+    <li>melting point decreases</li>
+    <li>reactivity increases</li>
+  </ul>
+  <h3>Group 7</h3>
+  <p>This is a collection of nonmetals and are also known as halogens.</p>
+  <p>Going down a group:</p>
+  <ul>
+    <li>boiling point decreases</li>
+    <li>reactivity decreases</li>
+  </ul>
 </section>
