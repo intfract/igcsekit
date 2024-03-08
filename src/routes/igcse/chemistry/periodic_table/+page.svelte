afac5260912a73b68b75bbedd312d88a7230180e
Int Fract <intfract@gmail.com>
2024-03-08 07:34:42 +0000

    added periodic trends

--- src/routes/igcse/chemistry/periodic_table/+page.svelte
+++ src/routes/igcse/chemistry/periodic_table/+page.svelte
@@ -7,4 +7,6 @@
   <h2>The Periodic Table</h2>
   <p>The periodic table is used as a method of classifying and predicting the properties of elements.</p>
   <h2>Periodic Trends</h2>
+  <p>Elements become more non-metallic across a period. Elements near the bottom left corner of the table are the most metallic while elements near the top right corner are the least metallic.</p>
+  <p>An element's group number is equal to the number of electrons in its outer shell (valence electrons). For example, oxygen is in group 6 and has 6 valence electrons.</p>
 </section>
