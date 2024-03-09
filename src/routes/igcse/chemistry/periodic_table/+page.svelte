08f92c415681e3499d475a814c9b8a8d68b7e738
Int Fract <intfract@gmail.com>
2024-03-09 03:08:04 +0000

    edited periodic table chapter

--- src/routes/igcse/chemistry/periodic_table/+page.svelte
+++ src/routes/igcse/chemistry/periodic_table/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  //
+  import Callout from '$lib/components/Callout.svelte'
 </script>
 
 <section>
@@ -18,10 +18,24 @@
     <li>reactivity increases</li>
   </ul>
   <h3>Group 7</h3>
-  <p>This is a collection of nonmetals and are also known as halogens.</p>
+  <p>This is a collection of diatomic nonmetals and are also known as halogens.</p>
   <p>Going down a group:</p>
   <ul>
     <li>boiling point decreases</li>
     <li>reactivity decreases</li>
+    <li>colour darkens</li>
   </ul>
+  <h3>Transition Elements</h3>
+  <p>This is a collection of metals that:</p>
+  <ul>
+    <li>have high melting points</li>
+    <li>have high densities</li>
+    <li>form coloured compounds</li>
+    <li>often act as catalysts</li>
+  </ul>
+  <h3>Noble Gases</h3>
+  <p>This is a collection of unreactive monoatomic gases that belong to group 8. These gases are unreactive because their valence (outer) shell is full. This means they rarely form compounds.</p>
+  <Callout emoji="🤔">
+    Noble gases are also called group 0 sometimes.
+  </Callout>
 </section>
