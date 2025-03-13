680dde562ec4aad45605a130ee831deb56e9c268
Pranav Balaji Pooruli <pranav.pooruli@gmail.com>
2025-03-14 00:15:38 +0530

    chemistry: improved apparatus list, added aluminum extraction notes, and improved periodic table notes, condensation polymerization notes, and metals notes

--- src/routes/igcse/chemistry/+page.svelte
+++ src/routes/igcse/chemistry/+page.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
-  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table'
-  import LinkTree from '$lib/components/LinkTree.svelte'
+  import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
+  import LinkTree from "$lib/components/LinkTree.svelte";
 
-  export let data
+  export let data;
 
   let rows: [string, boolean, boolean, boolean][] = [
-    ['Experimental Techniques', true, true, true],
-    ['Stoichiometry', true, true, true],
-    ['Reactions', true, true, true],
-    ['Electrolysis', true, true, true],
-    ['Periodic Table', true, true, true],
-    ['Metals', true, true, true],
-    ['Organic Chemistry', true, true, true],
-  ]
+    ["Experimental Techniques", true, true, true],
+    ["Stoichiometry", true, true, true],
+    ["Reactions", true, true, true],
+    ["Electrolysis", true, true, true],
+    ["Periodic Table", true, true, true],
+    ["Metals", true, true, true],
+    ["Organic Chemistry", true, true, true],
+  ];
 
-  let page = 'IGCSE Chemistry'
+  let page = "IGCSE Chemistry";
 </script>
 
 <svelte:head>
   <title>{page}</title>
-  <meta property="og:title" content={page}>
-  <meta property="og:description" content={`Read ${page} notes and answer topic questions for free!`}>
+  <meta property="og:title" content={page} />
+  <meta
+    property="og:description"
+    content={`Read ${page} notes and answer topic questions for free!`}
+  />
 </svelte:head>
 
 <section>
   <h1>Chemistry</h1>
   <h2>Syllabus</h2>
-  <p>The double and combined column refer to <a href="https://www.cambridgeinternational.org/Images/597069-2023-2024-syllabus.pdf" target="_blank">co-ordinated sciences</a> and <a href="https://www.cambridgeinternational.org/images/557017-2022-syllabus-.pdf" target="_blank">combined sciences</a> respectively.</p>
+  <p>
+    The double and combined column refer to <a
+      href="https://www.cambridgeinternational.org/Images/597069-2023-2024-syllabus.pdf"
+      target="_blank">co-ordinated sciences</a
+    >
+    and
+    <a
+      href="https://www.cambridgeinternational.org/images/557017-2022-syllabus-.pdf"
+      target="_blank">combined sciences</a
+    > respectively.
+  </p>
   <DataTable>
     <Head>
       <Row>
@@ -39,12 +52,21 @@
     <Body>
       {#each rows as row}
         <Row>
-          {#each row as cell,i}
+          {#each row as cell, i}
             <Cell>
-              {#if i === 0 && typeof cell === 'string'}
-                <a href={[data.pathname, cell.replaceAll(' ', '_').toLowerCase()].join('/')}>{cell}</a>
+              {#if i === 0 && typeof cell === "string"}
+                <a
+                  href={[
+                    data.pathname,
+                    cell.replaceAll(" ", "_").toLowerCase(),
+                  ].join("/")}>{cell}</a
+                >
               {:else}
-                <span class="material-symbols-rounded" style="text-align: center; width: 100%;">{cell ? 'check' : 'close'}</span>
+                <span
+                  class="material-symbols-rounded"
+                  style="text-align: center; width: 100%;"
+                  >{cell ? "check" : "close"}</span
+                >
               {/if}
             </Cell>
           {/each}
@@ -53,17 +75,23 @@
     </Body>
   </DataTable>
   <h2>Practical</h2>
-  <p>The <strong>alternative to practical</strong> paper assesses practical skills and includes writing about experiments.</p>
+  <p>
+    The <strong>alternative to practical</strong> paper assesses practical skills
+    and includes writing about experiments.
+  </p>
   <h3>Apparatus</h3>
   <ul>
-    <li>Beaker</li>
-    <li>Conical Flask</li>
     <li>Test Tube</li>
+    <li>Conical Flask</li>
+    <li>Beaker</li>
+    <li>Gas Syringe</li>
     <li>Measuring Cylinder</li>
-    <li>Syringe</li>
-    <li>Pipette</li>
+    <li>Volumetric Pipette</li>
+    <li>Dropping/teat Pipette</li>
+    <li>Burette</li>
     <li>Glass Rod</li>
     <li>Delivery Tube</li>
   </ul>
-  <LinkTree url={data.pathname} glob={import.meta.glob('./**/+page.svelte')}></LinkTree>
+  <LinkTree url={data.pathname} glob={import.meta.glob("./**/+page.svelte")}
+  ></LinkTree>
 </section>
